0648920fa79b64c9d6bfe9538c396def619d66ff
tumbashka <tumbashka@gmail.com>
2019-12-12 23:25:57 +0400

    laba1

--- Buharov_lab/Program.cs
+++ Buharov_lab/Program.cs
@@ -16,7 +16,7 @@ namespace Buharov_lab
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormBulldozer());
         }
     }
 }
