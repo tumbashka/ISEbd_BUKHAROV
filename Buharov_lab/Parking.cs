21fddd703e595d39f123719a92c7688081788d98
tumbashka <tumbashka@gmail.com>
2019-12-27 13:05:10 +0400

    a

--- Buharov_lab/Parking.cs
+++ Buharov_lab/Parking.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Buharov_lab
 {
-    class Parking<T> where T : class, ITransport
+    public class Parking<T> : IEnumerator<T>, IEnumerable<T>, IComparable<Parking<T>> where T : class, ITransport
     {
         private Dictionary<int, T> _places;
 
@@ -20,6 +21,16 @@ namespace Buharov_lab
         private const int _placeSizeWidth = 210;
 
         private const int _placeSizeHeight = 80;
+
+        private int _currentIndex;
+
+        public int GetKey
+        {
+            get
+            {
+                return _places.Keys.ToList()[_currentIndex];
+            }
+        }
 
         public Parking(int sizes, int pictureWidth, int pictureHeight)
         {
@@ -35,6 +46,10 @@ namespace Buharov_lab
             {
                 throw new ParkingOverflowException();
             }
+            if (p._places.ContainsValue(car))
+            {
+                throw new ParkingAlreadyHaveException();
+            }
             for (int i = 0; i < p._maxCount; i++)
             {
                 if (p.CheckFreePlace(i))
@@ -112,6 +127,89 @@ namespace Buharov_lab
                     throw new ParkingOccupiedPlaceException(ind);
                 }
             }
+        }
+        public T Current
+        {
+            get
+            {
+                return _places[_places.Keys.ToList()[_currentIndex]];
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Dispose()
+        {
+            _places.Clear();
+        }
+
+        public bool MoveNext()
+        {
+            if (_currentIndex + 1 >= _places.Count)
+            {
+                Reset();
+                return false;
+            }
+            _currentIndex++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int CompareTo(Parking<T> other)
+        {
+            if (_places.Count > other._places.Count)
+            {
+                return -1;
+            }
+            else if (_places.Count < other._places.Count)
+            {
+                return 1;
+            }
+            else if (_places.Count > 0)
+            {
+                var thisKeys = _places.Keys.ToList();
+                var otherKeys = other._places.Keys.ToList();
+                for (int i = 0; i < _places.Count; ++i)
+                {
+                    if (_places[thisKeys[i]] is Tractor && other._places[thisKeys[i]] is Bulldozer)
+                    {
+                        return 1;
+                    }
+                    if (_places[thisKeys[i]] is Bulldozer && other._places[thisKeys[i]] is Tractor)
+                    {
+                        return -1;
+                    }
+                    if (_places[thisKeys[i]] is Tractor && other._places[thisKeys[i]] is Tractor)
+                    {
+                        return (_places[thisKeys[i]] is Tractor).CompareTo(other._places[thisKeys[i]] is Tractor);
+                    }
+                    if (_places[thisKeys[i]] is Bulldozer && other._places[thisKeys[i]] is Bulldozer)
+                    {
+                        return (_places[thisKeys[i]] is Bulldozer).CompareTo(other._places[thisKeys[i]] is Bulldozer);
+                    }
+                }
+            }
+            return 0;
         }
     }
 }
